f38539d7a25699252eec495b7d6d6129a0989d38
Zachary Berry <zberry92@gmail.com>
2012-09-30 05:18:25 -0400

    Completed Program 2a
    - Cleaned up the code a lot.
    - Added enums to determine our suit.
    - Added last operation overload and exceptions.
    - It's late and I hate everyone.

--- program2/program2/program.cpp
+++ program2/program2/program.cpp
@@ -1,110 +1,196 @@
+/* Zachary Berry and Patrick Willett (berwil)
+ * This program will create a deck object then create a linked list of 52 cards in the deck. All of the cards
+ * will then be printed with overloaded operators.
+ */
+
 #include <iostream>
+#include "d_except.h"
 #include "d_node.h"
 
 using namespace std;
 
+// enum to define values for our suits.
+enum card_suits_t {CLUB, 
+				   HEART, 
+				   DIAMOND, 
+				   SPADE};
+
+// Card class to handle the name and suit of a specific card.
 class card
 {
 public:
-	card(){};
-	card(const int x, const int y){suit = y; value = x;};  //card(int suit, int value) 
-	int getSuit(){return suit;};
-	int getValue(){return value;};
-	void setSuit(const int x){suit = x;};
-	void setValue(const int x){value = x;};
+	card();
+	card(const int value, const int suit);
+	const int getValue() {return cardValue;};
+	const int getSuit() {return cardSuit;};
+	void setValue(const int value);
+	void setSuit(const int suit);
 
 private:
-	int suit; /* 1:club, 2:diamond  3:heart  4:spade */
-	int value; /* jack:11, queen:12, king:13, ace: 14 */
+	int cardValue; //For cards above 10: 11 - Jack, 12 - Queen, 13 - King, 14 - Ace
+	int cardSuit;  //Values determined by enum card_suits_t 
 };
 
+// Deck class to make the deck and hold the memory positions of the firt and last card in the
+// linked list of card objects.
 class deck
 {
 public:
 	deck();
-	~deck(){};
+	~deck();
+	friend ostream& operator<< (ostream& ostr, deck& tempDeck);
 	void makeDeck();
-	void printDeck();
 
 private:
 	node<card> *front;
+	node<card> *last;
 };
 
-ostream& operator<< (ostream& os , card& a) {
-
-		  os<<"Value: ";
-		  if(a.getValue() == 14)
-			  os<<"Ace";
-		  else if(a.getValue() == 13)
-			  os<<"King";
-		  else if(a.getValue() == 12)
-			  os<<"Queen";
-		  else if(a.getValue() == 11)
-			  os<<"Jack";
-		  else 
-			  os<<a.getValue();
-
-			  
-		  cout<<" Suit: ";
-		  
-		  switch(a.getSuit())
-		  {
-			  case (1):  //club
-				  os<<"Club ";
-				  break;
-			  case(2):	//diomoind
-				  os<<"Diamond ";
-				  break;
-			  case(3):	//heart
-				  os<<"Heart ";
-				  break;
-			  case(4):	//spade
-				  os<<"Spade ";
-				  break;
-		  }
-		  os<<endl;
-			
-
-		  return os;
-		}
-
-
-deck::deck()
+// Overloaded operator to print the value and suit of the card object.
+ostream& operator<< (ostream& ostr, card& tempCard) 
 {
-	front = NULL;
+	//Print the value of the card.
+	ostr<<"Your card is a : ";
+	if (tempCard.getValue() == 14)
+	{
+		ostr <<"Ace";
+	}
+	else if(tempCard.getValue() == 13)
+	{
+		ostr <<"King";
+	}
+	else if(tempCard.getValue() == 12)
+	{
+		ostr <<"Queen";
+	}
+	else if(tempCard.getValue() == 11)
+	{
+		ostr <<"Jack";
+	}
+	else 
+	{
+		ostr<<tempCard.getValue();
+	}		  
+	
+	//Print the suit of the card
+	ostr<<" of ";
+	switch(tempCard.getSuit())
+	{
+	case (CLUB):
+		ostr <<"Clubs";
+		break;
+	case (HEART):
+		ostr <<"Hearts";
+		break;
+	case (DIAMOND):
+		ostr <<"Diamonds";
+		break;
+	case (SPADE):	
+		ostr <<"Spades";
+		break;
+	}
+
+	ostr <<endl;
+	return ostr;
 }
 
+// Overloaded operator to print out an entire deck linked list pointed to by the deck object.
+ostream& operator<< (ostream& ostr, deck& tempDeck)
+{
+	node<card> *traverse;
+	traverse = tempDeck.front;
+
+	while (traverse != NULL)
+	{
+		ostr <<traverse->nodeValue;
+		ostr <<endl;
+
+		traverse = traverse->next;
+	}
+
+	return ostr;
+}
+
+// deck constructor
+deck::deck()
+{
+	makeDeck();
+}
+
+// deck destructor
+deck::~deck()
+{
+	delete front;
+	delete last;
+}
+
+// makeDeck() function will make a deck of 52 cards from 2 - Ace of each suit. Makes use of the node class. 
+// The card object is made into a node via the template class and is constructed into a linked list.
 void deck::makeDeck()
 {
 	node<card> *curr = new node<card>;
+	if (curr == NULL)
+	{
+		throw memoryAllocationError("deck::makeDeck(): Memory not allocated correctly.");
+	}
 	front = curr;
 
 	for (int i = 2; i < 15; i++)
 	{
-		for (int j = 1; j < 5; j++)
+		for (int j = 0; j < 4; j++)
 		{
 			card tempCard(i, j);
 			curr->nodeValue = tempCard;
-			if (!(i == 14 && j == 4))
+			// As long as we have not reached the end of the list keep creading nodes.
+			if (!(i == 14 && j == 3)) 
 			{
 				curr->next = new node<card>;
 				curr = curr->next;
+			}
+			// When we have reached the end of our cards set our last pointer to the end of the deck.
+			else
+			{
+				last = curr;
 			}
 		}
 	}
 }
 
-void deck::printDeck()
+// Default card constructor
+card::card()
 {
-	node<card> *traverse;
-	traverse = front;
+	cardValue = 0;
+	cardSuit = 0;
+}
 
-	while (traverse != NULL)
+// card constructor
+card::card(const int value, const int suit)
+{
+	cardValue = value;
+	cardSuit = suit;
+}
+
+void card::setValue(const int value)
+{
+	if (value < 2 && value > 14)
 	{
-		cout <<traverse->nodeValue;
-		cout <<endl;
+		throw rangeError("card::setValue() entered a number not in range.");
+	}
+	else
+	{
+		cardValue = value;
+	}
+}
 
-		traverse = traverse->next;
+void card::setSuit(const int suit)
+{
+	if (suit < 2 && suit > 14)
+	{
+		throw rangeError("card::setValue() entered a number not in range.");
+	}
+	else
+	{
+		cardSuit = suit;
 	}
 }
 
@@ -112,8 +198,24 @@ int main()
 {
 	deck myDeck;
 
-	myDeck.makeDeck();
-	myDeck.printDeck();
+	try
+	{
+		myDeck.makeDeck();
+	}
+
+	// Catch any errors that may be thrown.
+	catch (const rangeError& re)
+	{
+		cerr <<re.what() <<endl;
+	}
+	catch (const memoryAllocationError& re)
+	{
+		cerr <<re.what() <<endl;
+	}
+
+	// Print out the contents of the deck.
+	cout <<myDeck;
 
 	system("pause");
+	return 0;
 }
