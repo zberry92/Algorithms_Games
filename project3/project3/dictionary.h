e823ae7d059ce259025c2d1beea1251328f39094
Zachary Berry <zberry92@gmail.com>
2012-10-20 23:02:54 -0400

    Added quickSort and binSearch - not working!

--- project3/project3/dictionary.h
+++ project3/project3/dictionary.h
@@ -13,13 +13,15 @@ class dictionary{
 public:
 	dictionary();
 	void importWords();
-	int binSearch(const vector<string>ourWords, int first, int last, string target);
+	int partition(vector<string> ourWords, int left, int right);
+	void quickSort(vector<string> ourWords, int left, int right);
+	int binSearch(const vector<string> ourWords, int first, int last, string target);
 	int getMaxWordSize() const;
 	bool verifyWord(string str);
 	void checkWord(string str);
 
 private:
-	int maxWordLength, fFlag, rFlag;
+	int maxWordLength, totalWords, fFlag, rFlag;
 	vector<string> words;
 };
 
@@ -27,6 +29,7 @@ private:
 dictionary::dictionary()
 {
 	maxWordLength = 0;
+	totalWords = 0;
 	fFlag = 0;
 	rFlag = 0;
 }
@@ -62,11 +65,45 @@ void dictionary::importWords()
 		}
 	}
 
+	totalWords = numWords;
 	std::cout <<"Loading complete!" <<endl;
 	return;
 }
 
-int binSearch(const vector<string>ourWords, int first, int last, string target)
+int dictionary::partition(vector<string> ourWords, int left, int right)
+{
+  int position;
+  string str;
+
+  str = ourWords[left];
+  start = left;
+  
+  for (int i = start + 1; i <= right; i++)
+    {
+      if (ourWords[i].compare(str) < 0)
+	{
+	  swap(ourWords[position + 1], ourWords[i]);
+	  swap(ourWords[position], ourWords[position + 1]);
+	  position++;
+	}
+    }
+
+  return position;
+}
+
+void dictionary::quickSort(vector<string> ourWords, int left, int right)
+{
+  int p;
+
+  if (left < right)
+    {
+      p = partition(ourWords, left, right);
+      quickSort(ourWords, left, p - 1);
+      quickSort(ourWords, p + 1, right);
+    }
+}
+
+int binSearch(const vector<string> ourWords, int first, int last, string target, int compFlag)
 {
 	int midPoint;
 	string midStr;
@@ -75,7 +112,14 @@ int binSearch(const vector<string>ourWords, int first, int last, string target)
 	{
 		midPoint = (first + last) / 2;
 		midStr = ourWords[midPoint];
-
+		if (compFlag)
+		{
+		  if (midStr.compare(0, 5, target) == 0)
+		    {
+		      return 1;
+		    }
+		}
+		 
 		if (target == midStr)
 		{
 			return midPoint;
@@ -105,21 +149,15 @@ bool dictionary::verifyWord(string str)
 {
 	string revStr;
 
-	for (int i = 0; i < (int) words.size(); i++)
+        if (binSearch(words, 0, totalWords, str, 1) == 1)
 	{
-		if (words[i].compare(0, 5, str) == 0)
-		{
-				fFlag = 1;
-		}
+	       fFlag = 1;
 	}
 
 	revStr = string(str.rbegin(), str.rend());
-	for (int i = 0; i < (int) words.size(); i++)
+	if (binSearch(words, 0, totalWords, revStr, 1) == 1)
 	{
-		if (words[i].compare(0, 5, revStr) == 0)
-		{
-				rFlag = 1;
-		}
+	       rFlag = 1;
 	}
 
 	if (fFlag == 0 && rFlag == 0)
@@ -136,28 +174,25 @@ bool dictionary::verifyWord(string str)
 void dictionary::checkWord(string str)
 {	
 	string  revStr;
+	int match;
 
 	if (fFlag == 1)
 	{
-		for (int i = 0; i < (int) words.size(); i++)
-		{
-			if (str == words[i])
-			{
-					cout <<str <<endl;
-			}
-		}
+	        match = binSearch(words, 0, totalWords, str, 0);
+	        if (match != -1)
+	        {
+		                       cout <<str <<endl;
+	        }
 	}
 
 	if (rFlag == 1)
 	{
 		revStr = string(str.rbegin(), str.rend());
-		for (int i = 0; i < (int) words.size(); i++)
-		{
-			if (revStr == words[i])
-			{
-					cout <<revStr <<endl;
-			}
-		}
+	        match = binSearch(words, 0, totalWords, revStr, 0);
+	        if (match != -1)
+	        {
+		                       cout <<revStr <<endl;
+	        }
 	}
 
 	return;
