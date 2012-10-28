a9a6431af2f1b6cde5602f3e5fe4660c270669ef
Zachary Berry <zberry92@gmail.com>
2012-10-28 14:29:57 -0400

    Seperated class

---
+++ program4/board.h
@@ -0,0 +1,14 @@
+/* 
+ * Board class to handle all board operations.
+ */
+#ifndef BOARD_CLASS
+#define BOARD_CLASS
+
+#include <iostream>
+#include <limits.h>
+#include "d_matrix.h"
+#include "d_except.h"
+#include <list>
+#include <fstream>
+
+#endif
