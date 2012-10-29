50ca76b494ca856b5325840e5fd63d8963ed9f9a
Zachary Berry <zberry92@gmail.com>
2012-10-28 23:21:14 -0400

    Move Classes

--- program4/program.cpp
+++ program4/program.cpp
@@ -6,12 +6,7 @@
  * conflicts if any exist.
  */
 
-#include <iostream>
-#include <limits.h>
-#include "d_matrix.h"
-#include "d_except.h"
-#include <list>
-#include <fstream>
+#include "board.h"
 
 using namespace std;
 
@@ -29,54 +24,6 @@ const int MinValue = 1;
 const int MaxValue = 9;
 
 int numSolutions = 0;
-
-class board
-// Stores the entire Sudoku board
-{
-   public:
-      board(int);
-      void clear();
-      void initialize(ifstream &fin);
-      void print();
-      bool isBlank(int, int);
-      ValueType getCell(int, int);
-      
-   private:
-
-      // The following matrices go from 1 to BoardSize in each
-      // dimension.  I.e. they are each (BoardSize+1) X (BoardSize+1)
-
-      matrix<ValueType> value;
-};
-
-board::board(int sqSize)
-   : value(BoardSize+1,BoardSize+1)
-// Board constructor
-{
-}
-
-void board::clear()
-{
-}
-
-void board::initialize(ifstream &fin)
-// Read a Sudoku board from the input file.
-{
-   char ch;
-
-   clear();
-   for (int i = 1; i <= BoardSize; i++)
-      for (int j = 1; j <= BoardSize; j++)
-      {
-	 fin >> ch;
-
-         // If the read char is not Blank
-	 if (ch != '.')
-	 {
-            setCell(i,j,ch-'0');   // Convert char to int
-	 }
-      }
-}
 
 int squareNumber(int i, int j)
 // Return the square number of cell i,j (counting from left to right,
@@ -97,55 +44,7 @@ ostream &operator<<(ostream &ostr, vector<int> &v)
    return ostr;
 }
 
-ValueType board::getCell(int i, int j)
-// Returns the value stored in a cell.  Throws an exception
-// if bad values are passed.
-{
-   if (i >= 1 && i <= BoardSize && j >= 1 && j <= BoardSize)
-      return value[i][j];
-   else
-      throw rangeError("bad value in getCell");
-}
 
-bool board::isBlank(int i, int j)
-// Returns true if cell i,j is blank, and false otherwise.
-{
-   if (i < 1 || i > BoardSize || j < 1 || j > BoardSize)
-      throw rangeError("bad value in setCell");
-}
-
-void board::print()
-// Prints the current board.
-{
-   for (int i = 1; i <= BoardSize; i++)
-   {
-      if ((i-1) % SquareSize == 0)
-      {
-         cout << " -";
-	 for (int j = 1; j <= BoardSize; j++)
-	    cout << "---";
-         cout << "-";
-	 cout << endl;
-      }
-      for (int j = 1; j <= BoardSize; j++)
-      {
-	 if ((j-1) % SquareSize == 0)
-	    cout << "|";
-	 if (!isBlank(i,j))
-	    cout << " " << getCell(i,j) << " ";
-	 else
-	    cout << "   ";
-      }
-      cout << "|";
-      cout << endl;
-   }
-
-   cout << " -";
-   for (int j = 1; j <= BoardSize; j++)
-      cout << "---";
-   cout << "-";
-   cout << endl;
-}
 
 int main()
 {
